c2884a7aab962e5710be9424587e1f2364a34f4f
Juhasz Zoltan <juhaszzoltanmate@yahoo.com>
2022-01-12 16:29:06 +0100

    Add project files.

---
+++ Futo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA220112
+{
+    internal class Futo
+    {
+        public string Nev { get; set; }
+        public int Rajtszam { get; set; }
+        public bool Kategoria { get; set; }
+        public TimeSpan Ido { get; set; }
+        public int TavSzazalek { get; set; }
+
+        public Futo(string sor)
+        {
+            var v = sor.Split(';');
+            Nev = v[0];
+            Rajtszam = int.Parse(v[1]);
+            Kategoria = v[2] == "Ferfi";
+            var t = v[3].Split(':');
+            Ido = new TimeSpan(
+                hours: int.Parse(t[0]),
+                minutes: int.Parse(t[1]),
+                seconds: int.Parse(t[2]));            
+            TavSzazalek = int.Parse(v[4]);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA220112
+{
+    internal class Program
+    {
+        static List<Futo> futok = new List<Futo>();
+        static void Main()
+        {
+            F02();
+            F03();
+            Console.ReadKey(true);
+        }
+
+        private static void F03()
+        {
+            Console.WriteLine($"3. feladat: Egyéni indulók száma: {futok.Count} fő");
+        }
+
+        private static void F02()
+        {
+            using (var sr = new StreamReader(@"..\..\res\ub2017egyeni.txt", Encoding.UTF8))
+            {
+                var fejlec = sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    futok.Add(new Futo(sr.ReadLine()));
+                }
+            }
+        }
+    }
+}
